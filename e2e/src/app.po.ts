9b280c3f4275e25bc109d52e534c3dfee25a42c6
Michael Solati <mkslt04@gmail.com>
2019-01-04 18:15:22 -0800

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getTitleText() {
+    return element(by.css('nge-root h1')).getText();
+  }
+}
